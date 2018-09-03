d5c8949f9e221e618de32761d535455ebabde16e
liuyq <liuyq@jingoal.com>
2018-09-03 16:58:35 +0800

    build for arm9

--- build.sh
+++ build.sh
@@ -6,8 +6,13 @@ SOURCE=$ROOT/build.temp/ffmpeg-4.0.2
 
 NDK=~/Library/Android/sdk/ndk-bundle
 SYSROOT=$NDK/platforms/android-26/arch-arm/
+
+#如果是 linux，则 darwin-x86_64 改为 linux-x86_64, 没有环境，未验证
 TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
-CPU=arm
+
+
+
+CFLAGS='-I$NDK/sysroot/usr/include -I$NDK/sysroot/usr/include/arm-linux-androideabi'
 
 if [ ! -d build.temp ]; then
     echo make build.temp
@@ -18,26 +23,30 @@ pushd build.temp
 
 if [ ! -d ffmpeg-4.0.2 ]; then
     if [ ! -f ffmpeg-4.0.2.tar.bz2 ]; then
-        wget http://ffmpeg.org/releases/ffmpeg-4.0.2.tar.bz2 && tar -xf ffmpeg-4.0.2.tar.bz2
+        wget http://ffmpeg.org/releases/ffmpeg-4.0.2.tar.bz2
         if [ $? -ne 0 ]; then
             echo Failed to download ffmpeg-4.0.2.tar.bz2
             exit -1
         fi
     fi
+
+    tar -xf ffmpeg-4.0.2.tar.bz2
 fi
 
-if [ ! -d temp ]; then
-    mkdir temp
-fi
+pushd ffmpeg-4.0.2
 
-pushd temp
+ARCH=arm
+CPU=arm9
 
-"$SOURCE/configure" --prefix=$OUTPUT                                \
+cp -r $ROOT/patches/* .
+
+./configure --prefix=$OUTPUT                                        \
             --enable-cross-compile                                  \
             --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi-    \
             --target-os=linux                                       \
             --host-os=linux                                         \
-            --arch=$CPU                                             \
+            --arch=$ARCH                                            \
+            --cpu=$CPU                                              \
             --sysroot=$SYSROOT                                      \
             --enable-shared                                         \
             --disable-static                                        \
@@ -48,6 +57,7 @@ pushd temp
             --enable-nonfree                                        \
             --enable-gpl                                            \
             --disable-symver                                        \
+
 
 if [ $? -eq 0 ]; then
     make && make install
